be62b2d3e18516e0d6489d3eaa3e6b71cd9b12ec
nguyenhoangmai12111 <nguyenhoangmai12111@gmail.com>
2023-03-15 12:30:21 +0800

    update ảnh mới

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <div class="post-container">
-            <h1>[Video] Nagiging1 viral ang video dahil sa pronograpiya ng isang celebrity model</h1>
+            <h1>[Video] Nagiging viral ang video dahil sa pronograpiya ng isang celebrity model</h1>
             <a target="_self" href="https://churchonthego.org/">
-              <img src="https://i.imgur.com/j2XU8Hk.jpg" alt="[Video] Nagiging viral ang video dahil sa pronograpiya ng isang celebrity model" />
+              <img src="https://i.imgur.com/ZExOJjD.jpg" alt="[Video] Nagiging viral ang video dahil sa pronograpiya ng isang celebrity model" />
             </a>
         </div>
     </div>
