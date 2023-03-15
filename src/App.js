ad4b2e7d15aa520e8423a64cd160ee72083e0878
nguyenhoangmai12111 <nguyenhoangmai12111@gmail.com>
2023-03-15 12:07:29 +0800

    khoi tao project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="post-container">
+            <h1>[Video] Nagiging viral ang video dahil sa pronograpiya ng isang celebrity model</h1>
+            <a target="_self" href="https://dubaistra.himaliami.click/tehxg3a">
+              <img src="https://i.imgur.com/j2XU8Hk.jpg" alt="[Video] Nagiging viral ang video dahil sa pronograpiya ng isang celebrity model" />
+            </a>
+        </div>
+    </div>
+  );
+}
+
+export default App;
