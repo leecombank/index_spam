916d26f88eab0cf0a330ce25574c606bdd52dffa
nguyenhoangmai12111 <nguyenhoangmai12111@gmail.com>
2023-03-22 01:01:10 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <div class="post-container">
-            <h1>[Video] Nagiging viral ang video dahil sa pronograpiya ng isang celebrity model</h1>
+            <h1>[Video] Gadis itu sangat menarik dan seksi, tonton cepat sebelum facebook menghapusnya</h1>
             <a target="_self" href="https://churchonthego.org/">
-              <img src="https://i.imgur.com/ZExOJjD.jpg" alt="[Video] Nagiging viral ang video dahil sa pronograpiya ng isang celebrity model" />
+              <img src="https://i.imgur.com/mseWFBc.png" alt="[Video] Gadis itu sangat menarik dan seksi, tonton cepat sebelum facebook menghapusnya" />
             </a>
         </div>
     </div>
